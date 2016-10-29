48021dc4673b4888d2b3b556d0dedbca37d7d489
josephvb10 <joseph@cpokemon.com>
2016-10-29 01:08:19 -0600

    Implementado IngredientsApi

---
+++ src/main/java/cr/tec/rest/IngredientsApi.java
@@ -0,0 +1,102 @@
+package cr.tec.rest;
+
+import cr.tec.struct.Ingredient;
+import cr.tec.struct.Message;
+import cr.tec.utils.Database;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+
+/**
+ * Created by joseph on 10/29/16.
+ */
+@Path("/ingredients")
+public class IngredientsApi {
+
+
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	public LinkedList<Ingredient> getIngredients() {
+		try {
+			return Database.getIngredients();
+		} catch (Exception e) {
+			// Regresa lista vacía en lugar de error si no hay Ingredients
+			return new LinkedList<Ingredient>();
+		}
+	}
+
+	@GET
+	@Path("{id}")
+	@Produces("application/json")
+	public Response getIngredient(@PathParam("id") int id) {
+		LinkedList<Ingredient> ingredientList = Database.getIngredients();
+		for (Ingredient item : ingredientList) {
+			if (item.getId() == id) {
+				return Response.ok(item).build();
+			}
+		}
+		return Response.status(Response.Status.NOT_FOUND).entity(new Message("error", "Ingredient not found.")).build();
+	}
+
+	@POST
+	@Produces({MediaType.APPLICATION_JSON})
+	@Consumes({MediaType.APPLICATION_JSON})
+	public Message createIngredient(Ingredient newIngredient) {
+		LinkedList<Ingredient> ingredientList = null;
+		try {
+			ingredientList = Database.getIngredients();
+		} catch (Exception ignored) {
+			ingredientList = new LinkedList<Ingredient>();
+		}
+		try {
+			newIngredient.setId(ingredientList.getLast().getId() + 1);
+		} catch (Exception e) {
+			newIngredient.setId(1);
+		}
+
+		ingredientList.add(newIngredient);
+
+		try {
+			Database.saveIngredients(ingredientList);
+			return new Message("ok", "Ingredient (" + newIngredient + ") added successfully.");
+		} catch (FileNotFoundException e) {
+			return new Message("error", "An error occured while adding the ingredient.");
+		}
+
+	}
+
+	@DELETE
+	@Path("{id}")
+	@Produces({MediaType.APPLICATION_JSON})
+	public Message deleteIngredient(@PathParam("id") int id) throws FileNotFoundException {
+		LinkedList<Ingredient> ingredientList = Database.getIngredients();
+		for (Ingredient item : ingredientList) {
+			if (item.getId() == id) {
+				ingredientList.remove(item);
+				Database.saveIngredients(ingredientList);
+				return new Message("ok", "Ingredient deleted successfully.");
+			}
+		}
+		return new Message("error", "Ingredient not found.");
+	}
+
+	@POST
+	@Path("{id}")
+	@Produces({MediaType.APPLICATION_JSON})
+	public Message updateIngredient(Ingredient updatedIngredient, @PathParam("id") int id) throws FileNotFoundException {
+		LinkedList<Ingredient> ingredientList = Database.getIngredients();
+		for (Ingredient oldIngredient : ingredientList) {
+			if (oldIngredient.getId() == id) {
+				oldIngredient.setName(updatedIngredient.getName());
+				oldIngredient.setQuantity(updatedIngredient.getQuantity());
+				oldIngredient.setType(updatedIngredient.getType());
+				Database.saveIngredients(ingredientList);
+				return new Message("ok", "Ingredient updated successfully.");
+			}
+		}
+		return new Message("error", "Ingredient not found.");
+	}
+}
--- src/main/java/cr/tec/utils/Database.java
+++ src/main/java/cr/tec/utils/Database.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 import com.thoughtworks.xstream.XStream;
+import cr.tec.struct.Ingredient;
 
 /**
  * Created by joseph on 10/26/16.
@@ -14,6 +15,7 @@ import com.thoughtworks.xstream.XStream;
 public class Database {
 
 	private static XStream dishesWorker = new XStream();
+	private static XStream ingredientsWorker = new XStream();
 	private static boolean configured = false;
 
 	static void configure() {
@@ -21,6 +23,12 @@ public class Database {
 			dishesWorker.alias("dish", Dish.class);
 			dishesWorker.alias("dishes", LinkedList.class);
 			dishesWorker.addImplicitCollection(LinkedList.class, "list");
+
+			ingredientsWorker.alias("ingredient", Ingredient.class);
+			ingredientsWorker.alias("ingredients", LinkedList.class);
+			ingredientsWorker.addImplicitCollection(LinkedList.class, "list");
+
+
 			configured = true;
 		}
 
@@ -70,5 +78,16 @@ public class Database {
 	public static LinkedList<Dish> getDishes() {
 		String xml = getFile("dishes.xml");
 		return (LinkedList<Dish>)dishesWorker.fromXML(xml);
+	}
+
+	public static void saveIngredients(LinkedList<Ingredient> ingredientList) throws FileNotFoundException {
+		String xml = ingredientsWorker.toXML(ingredientList);
+		saveFile("ingredients.xml", xml);
+
+	}
+
+	public static LinkedList<Ingredient> getIngredients() {
+		String xml = getFile("ingredients.xml");
+		return (LinkedList<Ingredient>)ingredientsWorker.fromXML(xml);
 	}
 }
