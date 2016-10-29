9adf6845bc1b7ac68521c9268354dd8d11dcca87
josephvb10 <joseph@cpokemon.com>
2016-10-28 22:20:41 -0600

    Agregado un dish por defecto para que no de error

--- src/main/java/cr/tec/struct/FoodType.java
+++ src/main/java/cr/tec/struct/FoodType.java
@@ -4,5 +4,5 @@ package cr.tec.struct;
  * Created by joseph on 10/26/16.
  */
 public enum FoodType {
-	vegetable, meat, fruits, grains, milky;
+	vegetable, meat, fruits, grains, milky
 }
--- src/main/java/cr/tec/struct/OrderType.java
+++ src/main/java/cr/tec/struct/OrderType.java
@@ -7,7 +7,7 @@ public enum OrderType {
 	Platinum(1), Gold(2), Bronze(3);
 	private int value;
 
-	private OrderType(int value) {
+	OrderType(int value) {
 		this.value = value;
 
 	}
